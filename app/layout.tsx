f4ca828c2bd3d6ad2c158f8509166766c0e72b01
jigar-padhiyar <jigar.padhiyar@techsierra.in>
2025-03-11 19:11:37 +0530

    Integrated auth from next auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
-import type { Metadata } from "next";
+"use client";
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Provider } from "react-redux";
+import { store } from "@/state/store/store";
+import AuthProvider from "@/context/authProvider";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { useEffect, useState } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,22 +18,34 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Admin Dashboard",
-  description: "Admin dashboard for assessment test",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  // Handle dark mode
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem("theme") === "dark";
+
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
+
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+        <AuthProvider>
+          <Provider store={store}>
+            <QueryClientProvider client={queryClient}>
+              {children}
+            </QueryClientProvider>
+          </Provider>
+        </AuthProvider>
       </body>
     </html>
   );
