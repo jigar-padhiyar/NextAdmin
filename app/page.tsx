91fe546ee56b7a0435cee8c09147bf64cecf4f1e
jigar-padhiyar <jigar.padhiyar@techsierra.in>
2025-03-12 14:58:33 +0530

    updated readme file

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,9 @@ import React, { useEffect } from "react";
 import Dashboard from "./pages/dashboard/dashboard";
 import SignIn from "./pages/auth/signin/signIn";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 
 const Page = () => {
   const { status } = useSession();
-  const router = useRouter();
-  useEffect(() => {
-    // If not authenticated, redirect to login
-    if (status === "unauthenticated") {
-      router.push("/pages/auth/signin");
-    }
-  }, [status, router]);
   return status === "unauthenticated" ? <SignIn /> : <Dashboard />;
 };
 
